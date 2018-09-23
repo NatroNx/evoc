ba9a10a65120f613cab32829bf0b6f9551918085
NatroN <natron2e@gmail.com>
2018-09-23 21:15:51 +0200

    ignore credentials

--- evoc.ino
+++ evoc.ino
@@ -7,20 +7,7 @@ Software can be used as is and is licensed under GPLv3
 #include <FreematicsPlus.h>
 #include <WiFiClientSecure.h>
 #include <ThingerESP32.h>
-
-
-#define USERNAME "your_user_name"
-#define DEVICE_ID "your_device_id"
-#define DEVICE_CREDENTIAL "your_device_credential"
-
-#define SSID "your_wifi_ssid"
-#define SSID_PASSWORD "your_wifi_ssid_password" isPhoneGap
-onsce
-var isPhoneGap = false;
-
-
-
-
+#include "credentials.h"  //just a file where I Store my credentials in.
 
 
 
