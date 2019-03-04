ab4946bb2c18f631db0b4cb2a34a48280dc85160
NatroNx <NatroNx@users.noreply.github.com>
2019-03-04 23:18:09 +0100

    Delete credentials.h

--- credentials.h
+++
@@ -1,6 +0,0 @@
-#define USERNAME "ThingerUsername"
-#define DEVICE_ID "ThingerDeviceID"
-#define DEVICE_CREDENTIAL "THingerDeiceCredential"
-
-#define SSID "SSID"
-#define SSID_PASSWORD "YourSuperPrivatePassword" 
